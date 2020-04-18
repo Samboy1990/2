09ace06bce9d6d3b8ced76e6f23d2f1237b3b1c5
Samboy1990 <63283930+Samboy1990@users.noreply.github.com>
2020-04-18 11:33:13 +0430

    Initialize Big_endian

---
+++ src/wps/t6.h
@@ -0,0 +1,17 @@
+char cmd_pixie[770];
+char cmd_pixie_aux[770];
+
+char pixie_buf_aux[770];
+char pixie_buf_aux2[770];
+
+//fixed size
+char pixie_pke[570];					/* save pke */
+char pixie_pkr[570];					/* save pkr */
+char pixie_enonce[34];					/* save e-nonce */
+char pixie_rnonce[34];					/* save r-nonce */
+char pixie_authkey[68];				/* save AuthKey */
+char pixie_ehash1[66];					/* save e-hash1 */
+char pixie_ehash2[66];					/* save e-hash2 */
+	
+//int op_pixie=0;
+
